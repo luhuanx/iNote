15d815ca17fb6c08fcae3598b78e111bfb04a0e4
Mimanchi <1640247539@qq.com>
2022-06-01 19:59:41 +0800

    添加动画 优化用户体验

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,33 +6,38 @@
           <div class="main"></div>
           <div class="form">
             <h3 @click="showRegister">创建账户</h3>
-            <div v-show="isShowRegister"
-                 class="register">
-              <input type="text"
-                     v-model="register.username"
-                     placeholder="用户名">
-              <input type="password"
-                     v-model="register.password"
-                     placeholder="密码">
-              <p v-bind:class="{error:register.isError}">{{register.notice}}</p>
-              <div class="button"
-                   @click="onRegister">创建账号</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show:isShowRegister}"
+                   class="register">
+                <input type="text"
+                       v-model="register.username"
+                       placeholder="用户名">
+                <input type="password"
+                       v-model="register.password"
+                       @keyup.enter="onRegister"
+                       placeholder="密码">
+                <p v-bind:class="{error:register.isError}">{{register.notice}}</p>
+                <div class="button"
+                     @click="onRegister">创建账号</div>
+              </div>
+            </transition>
             <h3 @click="showLogin">登录</h3>
-            <div v-show='isShowLogin'
-                 class="login">
-              <input type="text"
-                     v-model="login.username"
-                     placeholder="请输入用户名">
-              <input type="password"
-                     v-model="login.password"
-                     placeholder="请输入密码">
-              <p v-bind:class="{error:login.isError}">{{login.notice}}</p>
-              <div class="button"
-                   @click="onLogin">登录</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show:isShowLogin}"
+                   class="login">
+                <input type="text"
+                       v-model="login.username"
+                       placeholder="请输入用户名">
+                <input type="password"
+                       v-model="login.password"
+                       @keyup.enter="onLogin"
+                       placeholder="请输入密码">
+                <p v-bind:class="{error:login.isError}">{{login.notice}}</p>
+                <div class="button"
+                     @click="onLogin">登录</div>
+              </div>
+            </transition>
           </div>
-
         </div>
       </div>
     </div>
@@ -164,6 +169,13 @@ export default {
     .register {
       padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height 0.4s;
+
+      &.show {
+        height: 193px;
+      }
 
       input {
         display: block;
